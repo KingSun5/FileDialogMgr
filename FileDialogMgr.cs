4621a69801014f08ca6260986aff02ac053c6d06
Sun <sunhao@hummergames.com>
2019-10-10 18:05:15 +0800

    初始化

---
+++ FileDialogMgr.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+
+/// <summary>
+/// time:2019/10/10  15：43
+/// author:Sun
+/// des:FileDialog 调取Win窗口的管理器
+///
+/// github:https://github.com/KingSun5
+/// csdn:https://blog.csdn.net/Mr_Sun88
+/// </summary>
+public class FileDialogMgr
+{
+    /// <summary>
+    /// 自定义打开文件的数据格式
+    /// </summary>
+    public OpenDialogData CustomOpenData;
+    /// <summary>
+    /// 自定义保存文件的数据格式
+    /// </summary>
+    public SaveDialogData CustomSaveData;
+    public FileDialogMgr()
+    {
+        InitFileDialogMgr();
+    }
+    /// <summary>
+    /// 初始化管理器的相关信息
+    /// </summary>
+    private void InitFileDialogMgr()
+    {
+        CustomOpenData = new OpenDialogData();
+        CustomOpenData.structSize = Marshal.SizeOf(CustomOpenData);
+        CustomOpenData.filter =  "All Files\0*.*\0\0";
+        //CustomOpenData.filter = "png图片\0*.png\0";
+        //"All Files\0*.*\0\0"  + "png图片\0*.png\0  过滤条件的格式
+        CustomOpenData.file = new string(new char[256]);
+        CustomOpenData.maxFile = CustomOpenData.file.Length;
+        CustomOpenData.fileTitle = new string(new char[1000]);
+        CustomOpenData.maxFileTitle = CustomOpenData.fileTitle.Length;
+        CustomOpenData.initialDir = Application.dataPath.Replace('/', '\\') + "\\aaa\\";
+        CustomOpenData.title = "打开项目";
+        CustomOpenData.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000200 | 0x00000008;
+
+    }
+
+    /// <summary>
+    /// 最基本的打开窗口
+    /// 不过滤任何文件
+    /// 可以打开任何格式文件
+    /// </summary>
+    /// <returns></returns> 和该文件相关的数据
+    public OpenDialogData OpenFileDlg()
+    {
+        if (OpenFileDialog.GetOpenFileName(CustomOpenData))
+        {
+            return CustomOpenData;
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// 设置单个过滤方式
+    /// </summary>
+    public void SetFilteringWay(EnumFilteringWay type)
+    {
+        CustomOpenData.filter = GetFilteringWay(type);
+    }
+
+    /// <summary>
+    /// 设置多个过滤方式
+    /// </summary>
+    /// <param name="types"></param>
+    public void SetFilteringWays(EnumFilteringWay[] types)
+    {
+        if(types==null||types.Length==1)
+        {
+            CustomOpenData.filter = "All Files\0*.*\0\0";
+            return;
+        }
+        CustomOpenData.filter = "";
+        for (int i = 0; i < types.Length; i++)
+        {
+            if (types[i]==EnumFilteringWay.All)
+            {
+                CustomOpenData.filter = "All Files\0*.*\0\0";
+                break;
+            }
+            CustomOpenData.filter += GetFilteringWay(types[i]);
+        }
+    }
+    private string GetFilteringWay(EnumFilteringWay type)
+    {
+        switch (type)
+        {
+            case EnumFilteringWay.Txt:
+                return "文本文件\0*.txt\0";
+            case EnumFilteringWay.Json:
+                return "Json文件\0*.json\0";
+            case EnumFilteringWay.Excel:
+                return "Excel文件\0*.xlsx\0";
+            case EnumFilteringWay.Png:
+                return "Png图片\0*.png\0";
+            case EnumFilteringWay.Jpg:
+                return "Jpg图片\0*.jpg\0";
+        }
+        return "All Files\0*.*\0\0";
+    }
+
+}
+
+public enum EnumFilteringWay
+{
+    All,
+    Txt,
+    Json,
+    Excel,
+    Png,
+    Jpg,
+}
---
+++ FileDlg.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+public class FileDialogData
+{
+    public int structSize = 0;       //结构的内存大小
+    public IntPtr dlgOwner = IntPtr.Zero;  //设置对话框的句柄
+    public IntPtr instance = IntPtr.Zero; //根据flags标志的设置，确定instance是谁的句柄，不设置则忽略
+    public String filter = null; //调取文件的过滤方式
+    public String customFilter = null; //一个静态缓冲区 用来保存用户选择的筛选器模式
+    public int maxCustFilter = 0; //缓冲区的大小
+    public int filterIndex = 0; //指向的缓冲区包含定义过滤器的字符串对
+    public String file = null; //存储调取文件路径
+    public int maxFile = 0; //存储调取文件路径的最大长度 至少256
+    public String fileTitle = null; //调取的文件名带拓展名
+    public int maxFileTitle = 0; //调取文件名最大长度
+    public String initialDir = null; //最初目录
+    public String title = null; //打开窗口的名字
+    public int flags = 0;//初始化对话框的一组位标志  参数类型和作用查阅官方API
+    public short fileOffset = 0; //文件名前的长度
+    public short fileExtension = 0;//拓展名前的长度
+    public String defExt = null;//默认的拓展名
+    public IntPtr custData = IntPtr.Zero;//传递给lpfnHook成员标识的钩子子程的应用程序定义的数据
+    public IntPtr hook = IntPtr.Zero;//指向钩子的指针。除非Flags成员包含OFN_ENABLEHOOK标志，否则该成员将被忽略。
+    public String templateName = null;//模块中由hInstance成员标识的对话框模板资源的名称
+    public IntPtr reservedPtr = IntPtr.Zero;
+    public int reservedInt = 0;
+    public int flagsEx = 0;//可用于初始化对话框的一组位标志
+}
+[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+public class OpenDialogData : FileDialogData
+{
+
+}
+
+/*这是C#引用非托管的C/C++的DLL的一种定义定义结构体的方式，主要是为了内存中排序，LayoutKind有两个属性Sequential和Explicit
+
+Sequential表示顺序存储，结构体内数据在内存中都是顺序存放的Explicit表示精确布局，需要用FieldOffset()设置每个成员的位置这都是
+
+为了使用非托管的指针准备的，知道什么意思就行，C#的CLR提供了更为灵活的自动管理方式，所以对C#来说可有可无。
+
+CharSet=CharSet.Ansi表示编码方式
+*/
+[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+public class SaveDialogData : FileDialogData
+{
+
+}
+
+public class OpenFileDialog
+{
+    [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+    public static extern bool GetOpenFileName([In, Out] OpenDialogData ofd);
+}
+public class SaveFileDialog
+{
+    [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+    public static extern bool GetSaveFileName([In, Out] SaveDialogData ofd);
+}
+
